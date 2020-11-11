a3796c491a2bbdb31f0dfefd30c90818975e0c7b
Ubuntu <ubuntu@ubuntu.lan>
2020-11-11 09:57:18 +0000

    fixed orb_slam2_ros/ORBState.h error

--- ORB_SLAM2/build.sh
+++ ORB_SLAM2/build.sh
@@ -1,35 +1,35 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
+#echo "Configuring and building Thirdparty/DBoW2 ..."
 
-cd orb_slam2_lib/Thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+#cd orb_slam2_lib/Thirdparty/DBoW2
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j2
 
-cd ..
-cd ../../g2o
+#cd ..
+#cd ../../g2o
 
-echo "Configuring and building Thirdparty/g2o ..."
+#echo "Configuring and building Thirdparty/g2o ..."
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j2
 
-cd ../../..
+#cd ../../..
 
-echo "Uncompress vocabulary ..."
+#echo "Uncompress vocabulary ..."
 
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ..
+#cd Vocabulary
+#tar -xf ORBvoc.txt.tar.gz
+#cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j2
 
 # back to the initial directory i.e. {WS}/src/ORB_SLAM2_ROS/ORB_SLAM2 (not necessary)
 cd ../..
